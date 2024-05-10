2d2cf4ee920c6a70d0f0eb61f66811ddd5fafdd3
UsterkoweOko <146815215+UsterkoweOko@users.noreply.github.com>
2024-05-10 20:23:21 +0200

    Delete Rofancier directory

--- Rofancier/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('applyStyle').addEventListener('click', () => {
-  const customCSS = document.getElementById('customCSS').value;
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { customCSS });
-  });
-});
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {action: 'inject'});
-});
